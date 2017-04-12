f8592f90e7293201077e71cc57e947fc968e550a
Tyler Merz <tyler.merz@gmail.com>
2017-04-12 10:07:31 -0700

    renamed InfoPage to infoPage

--- views/infoPage.jsx
+++ views/infoPage.jsx
@@ -1,23 +1,23 @@
-var React = require('react');
-var DefaultLayout = require('./layouts/default');
+var React = require('react')
+var DefaultLayout = require('./layouts/default')
 
 class InfoPage extends React.Component {
-    render() {
-        return (
-            <DefaultLayout title={this.props.message} >
-                <div className="mainBody">
-                    <div className="postMeat">
-                        <div className="postBody">
-                            <hr />
-                            <h1>Page Not Found.</h1>
-                            <p>
-                                {this.props.message}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </DefaultLayout>
-        )
-    }
+  render () {
+    return (
+      <DefaultLayout title={this.props.message} >
+        <div className='mainBody'>
+          <div className='postMeat'>
+            <div className='postBody'>
+              <hr />
+              <h1>Page Not Found.</h1>
+              <p>
+                {this.props.message}
+              </p>
+            </div>
+          </div>
+        </div>
+      </DefaultLayout>
+    )
+  }
 }
-module.exports = InfoPage;
+module.exports = InfoPage
